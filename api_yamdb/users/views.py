fe6dc0437c6df28f6d7397c2a0f8b5ddb40c0717
OpenedClosed <opendoor200179@gmail.com>
2022-07-16 00:40:43 +0300

    check the workflow

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,14 +1,15 @@
-from api_yamdb import settings
+from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from django.contrib.auth.tokens import default_token_generator
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
+
+from api_yamdb import settings
 from .models import CustomUser
 from .permissions import IsAdmin
 from .serializers import (CustomUserSerializer, SignUpSerializer,
